ee8f0df5ef6890ac0ede3a50884fb4d72df452c5
Michael Levan <mlevan1992@gmail.com>
2021-01-04 14:58:20 -0500

    JSON task definition and Python app to update the task definition

--- AWS-OctopusDeploy-Fargate/ECS-Fargate-Task-Definition/update_taskdefinition.py
+++ AWS-OctopusDeploy-Fargate/ECS-Fargate-Task-Definition/update_taskdefinition.py
@@ -1,3 +1,6 @@
+# Example:
+# python update_taskdefinition.py 'Server=ecshostedpoc.c1ufrgizkeyf.us-east-1.rds.amazonaws.com,1433;Initial Catalog=Octopus;Persist Security Info=False;User ID=sa;Password=S61#p!4tf7OLQCoV;MultipleActiveResultSets=False;Connection Timeout=30;'
+
 import json
 import sys
 
@@ -5,14 +8,18 @@ def main():
     update_taskdefinition(db_connection_string)
 
 def update_taskdefinition(db_connection_string):
-    # In progress
-
-    with open("Task-Definition-Terraform/task_definition.json") as j:
+    # The JSON file, "task_definition.json" gets read, along with the index for the environment
+    with open("Task-Definition-Terraform/task_definition.json", 'r+') as j:
         data = json.load(j)
-        data["containerDefinitions"]["environment"]["value"] = db_connection_string
-
-        j.write()
-
+        for d in data['containerDefinitions'][0]['environment']:
+            d['value'] = db_connection_string
+    
+    # After the index for the environment is read, the JSON file is written to
+    # with the connection string that you pass in at runtime
+    with open("Task-Definition-Terraform/task_definition.json", 'w') as input:
+        json.dump(data, input)
+        
+# Pass in the connection string at runtime
 db_connection_string = sys.argv[1]
 
 if __name__ == '__main__':
