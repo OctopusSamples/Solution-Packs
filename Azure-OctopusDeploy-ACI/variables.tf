e459beba4d1edf994b7c00a7f0e7c987895ab973
Michael Levan <mlevan1992@gmail.com>
2021-01-04 10:42:27 -0500

    ACI solution pack

---
+++ Azure-OctopusDeploy-ACI/variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  type = string
+}
+
+variable "RG" {
+  type = string
+}
+
+variable "storageAccountName" {
+  type = string
+}
+
+variable "octopusUser" {
+  type = string
+}
+
+variable "sqlLogin" {
+  type = string
+}
+
+variable "dbpassword" {
+  type = string
+}
+
+variable "octopusPassword" {
+  type = string
+}
+
+variable "databaseMasterKey" {
+  type = string
+}
